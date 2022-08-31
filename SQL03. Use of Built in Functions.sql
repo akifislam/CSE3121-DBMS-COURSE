4bc13412b793d9e07f8bbed5f6d20cded74d81c6
Akif Islam <iamakifislam@gmail.com>
2022-08-31 20:19:54 +0600

    triggered

---
+++ SQL03. Use of Built in Functions.sql
@@ -0,0 +1,12 @@
+-- Get the Maximum Fees among all
+-- SELECT  MAX(FEES) FROM STUDENT_FEE;
+
+-- Get the Average Fees of all
+-- SELECT avg(FEES) FROM STUDENT_FEE
+
+-- Get the Sum of All Fees
+-- SELECT SUM(FEES) FROM STUDENT_FEE; 
+
+
+-- Projection of a Column (Team)
+-- SELECT distinct team FROM STUDENT_FEE
